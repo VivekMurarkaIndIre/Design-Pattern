238d75437f0b88f20fb5c5018d4fc2dcce028c1e
Vivek Murarka <vivek.murarka@ucdconnect.ie>
2022-11-12 17:05:58 +0000

    AbstractFactoryExcercise:
    Exercise 1: We are implementing Scala object called Product. We use apply to decide which object needs to be created.
    There are three classes product A,B and C which extends Product abstract class and override doSomeWork() to call their own method
    Now our client is independent of specific product

---
+++ factorypattern/src/main/scala/factory.scala
@@ -0,0 +1,55 @@
+//
+// COMP40070 ABSTRACT FACTORY EXERCISE
+// 
+
+class Client:
+
+  var product: Product = Product("ProductA")
+
+  def foo() =
+    product.doSomeWork()
+    product = Product("ProductB")
+    product.doSomeWork()
+    product = Product("ProductC")
+    product.doSomeWork()
+
+
+abstract class Product{
+  //def createProduct(): Product
+  def doSomeWork(): Unit
+}
+
+object Product {
+  private class ProductA extends Product{
+    override def doSomeWork(): Unit = doYourStuff()
+    def doYourStuff() =
+      println("I'm a ProductA, doing my stuff")
+  }
+
+  private class ProductB extends Product {
+    override def doSomeWork(): Unit = doIt()
+
+    def doIt() =
+      println("I'm a ProductB, doing it")
+  }
+
+  private class ProductC extends Product {
+    override def doSomeWork(): Unit = perform
+    def perform =
+      println("I'm a ProductC, performing")
+  }
+
+  def apply(productName: String):Product = {
+    productName match {
+      case "ProductA" => new ProductA()
+      case "ProductB" => new ProductB()
+      case "ProductC" => new ProductC()
+    }
+  }
+}
+
+
+@main def main(): Unit =
+  val myClient = Client()
+  myClient.foo()
+  
