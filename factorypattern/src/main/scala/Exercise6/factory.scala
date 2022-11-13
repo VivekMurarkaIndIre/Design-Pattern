c77861a491e2923fff27ff73364bae1820e7bc8d
Vivek Murarka <vivek.murarka@ucdconnect.ie>
2022-11-13 19:57:01 +0000

    AbstractFactoryExcercise:
    Exercise 6:Replace existing factory with new factory.
    Done via companion object in scala

---
+++ factorypattern/src/main/scala/Exercise6/factory.scala
@@ -0,0 +1,237 @@
+package Exercise6
+
+
+//
+// COMP40070 ABSTRACT FACTORY EXERCISE
+//
+
+class Client(productFactory: ProductFactory):
+
+  var product:Product = productFactory.createProductA()
+
+  def foo() =
+    product.doSomeWork()
+    product = productFactory.createProductB()
+    product.doSomeWork()
+    product = productFactory.createProductC()
+    product.doSomeWork()
+    product = productFactory.createProductD()
+    product.doSomeWork()
+
+trait Product {
+  def doSomeWork(): Unit
+}
+
+abstract class ProductFactory{
+  def createProductA(): ProductA
+
+  def createProductB(): ProductB
+
+  def createProductC(): ProductC
+
+  def createProductD(): ProductD
+}
+
+object ProductFactory{
+  object CoolProductFactory extends ProductFactory {
+    override def createProductA(): ProductA = ProductA("CoolProductA")
+
+    override def createProductB(): ProductB = ProductB("CoolProductB")
+
+    override def createProductC(): ProductC = ProductC("CoolProductC")
+
+    override def createProductD(): ProductD = ProductD("CoolProductD")
+  }
+
+  object NormalProductFactory extends ProductFactory {
+    override def createProductA(): ProductA = ProductA("NormalProductA")
+
+    override def createProductB(): ProductB = ProductB("NormalProductB")
+
+    override def createProductC(): ProductC = ProductC("NormalProductC")
+
+    override def createProductD(): ProductD = ProductD("NormalProductD")
+  }
+
+  object DeadlyProductFactory extends ProductFactory {
+    override def createProductA(): ProductA = ProductA("DeadlyProductA")
+
+    override def createProductB(): ProductB = ProductB("DeadlyProductB")
+
+    override def createProductC(): ProductC = ProductC("DeadlyProductC")
+
+    override def createProductD(): ProductD = ProductD("DeadlyProductD")
+  }
+
+  def apply(productFactory: Int): ProductFactory = {
+    productFactory match {
+      case 1 => CoolProductFactory
+      case 2 => NormalProductFactory
+      case 3 => DeadlyProductFactory
+    }
+  }
+}
+abstract class ProductA extends Product{
+  def doSomeWork(): Unit
+
+}
+
+abstract class ProductB extends Product{
+  def doSomeWork(): Unit
+}
+
+abstract class ProductC extends Product{
+  def doSomeWork(): Unit
+}
+
+abstract class ProductD extends Product{
+  def doSomeWork(): Unit
+}
+
+object ProductA {
+  private class CoolProductA extends ProductA{
+    override def doSomeWork(): Unit = doYourStuff()
+    def doYourStuff(): Unit =
+      println("I'm a ProductA, doing my Cool stuff")
+  }
+
+  private class NormalProductA extends ProductA {
+    override def doSomeWork(): Unit = doYourStuff()
+
+    def doYourStuff(): Unit =
+      println("I'm a ProductA, doing my Normal stuff")
+  }
+
+  private class DeadlyProductA extends ProductA {
+    override def doSomeWork(): Unit = doYourStuff()
+
+    def doYourStuff(): Unit =
+      println("I'm a ProductA, doing my Deadly stuff")
+  }
+
+  def apply(productName: String):ProductA = {
+    productName match {
+      case "CoolProductA" => new CoolProductA()
+      case "NormalProductA" => new NormalProductA()
+      case "DeadlyProductA" => new DeadlyProductA()
+    }
+  }
+}
+
+object ProductB {
+  private class CoolProductB extends ProductB {
+    override def doSomeWork(): Unit = doIt()
+
+    def doIt(): Unit =
+      println("I'm a ProductB, doing it cool")
+  }
+
+  private class NormalProductB extends ProductB {
+    override def doSomeWork(): Unit = doIt()
+
+    def doIt(): Unit =
+      println("I'm a ProductB, doing it normally")
+  }
+
+  private class DeadlyProductB extends ProductB {
+    override def doSomeWork(): Unit = doIt()
+
+    def doIt(): Unit =
+      println("I'm a ProductB, doing it deadly")
+  }
+  def apply(productName: String):ProductB = {
+    productName match {
+      case "CoolProductB" => new CoolProductB()
+      case "NormalProductB" => new NormalProductB()
+      case "DeadlyProductB" => new DeadlyProductB()
+    }
+  }
+}
+
+object ProductC {
+
+  private class CoolProductC extends ProductC {
+    override def doSomeWork(): Unit = perform
+    def perform: Unit =
+      println("I'm a ProductC, performing cool stuff")
+  }
+
+  private class NormalProductC extends ProductC {
+    override def doSomeWork(): Unit = perform
+    def perform: Unit =
+      println("I'm a ProductC, performing normal stuff")
+  }
+
+  private class DeadlyProductC extends ProductC {
+    override def doSomeWork(): Unit = perform
+
+    def perform: Unit =
+      println("I'm a ProductC, performing deadly stuff")
+  }
+
+  def apply(productName: String):ProductC = {
+    productName match {
+      case "CoolProductC" => new CoolProductC()
+      case "NormalProductC" => new NormalProductC()
+      case "DeadlyProductC" => new DeadlyProductC()
+    }
+  }
+}
+
+object ProductD {
+
+  private class CoolProductD extends ProductD {
+    override def doSomeWork(): Unit = work
+    def work: Unit =
+      println("I'm a ProductD, performing cool stuff")
+  }
+
+  private class NormalProductD extends ProductD {
+    override def doSomeWork(): Unit = work
+    def work: Unit =
+      println("I'm a ProductD, performing normally")
+  }
+
+  private class DeadlyProductD extends ProductD {
+    override def doSomeWork(): Unit = work
+
+    def work: Unit =
+      println("I'm a ProductD, performing deadly stuff")
+  }
+
+  def apply(productName: String):ProductD = {
+    productName match {
+      case "CoolProductD" => new CoolProductD()
+      case "NormalProductD" => new NormalProductD()
+      case "DeadlyProductD" => new DeadlyProductD()
+    }
+  }
+}
+
+@main def main(): Unit =
+
+  var input = -1
+  var productFactory: ProductFactory = null
+  while (input != 0) {
+    println("Enter 1 for Cool, 2 for normal, 3 for deadly, 0 to exit ")
+    input = scala.io.StdIn.readInt()
+    if (input == 0) {
+      productFactory = null
+      println("Goodbye!!!")
+    }else if (input == 1) {
+      println("Creating Cool Products")
+      productFactory = ProductFactory(1)
+    } else if (input == 2) {
+      println("Creating Normal Products")
+      productFactory = ProductFactory(2)
+    } else if (input == 3) {
+      println("Creating Deadly Products")
+      productFactory = ProductFactory(3)
+    } else {
+      println("Invalid Input, Enter 0 to exit")
+    }
+    if(productFactory != null) {
+      val myClient = Client(productFactory)
+      myClient.foo()
+    }
+  }
